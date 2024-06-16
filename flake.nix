555f225c79f1adbe9bf805a7d91660897d53102d
casuallyblue <sierra@casuallyblue.dev>
2024-06-16 14:05:26 -0500

    Add flake and fix up script

---
+++ flake.nix
@@ -0,0 +1,81 @@
+{
+  description = "Build a cargo project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        inherit (pkgs) lib;
+
+        craneLib = crane.lib.${system}.overrideToolchain fenix.packages.${system}.complete.toolchain;
+        src = craneLib.path ./.;
+
+        commonBuildInputs = with pkgs; [
+          openssl
+          pkg-config
+          # Add additional build inputs here
+        ] ++ lib.optionals pkgs.stdenv.isDarwin
+          (with pkgs; with pkgs.darwin; [
+            # Additional darwin specific inputs can be set here
+            libiconv
+            Security
+          ]);
+
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+
+          buildInputs = commonBuildInputs;
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        };
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        my-crate = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
+        });
+      in
+      {
+        packages = {
+          default = my-crate;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = my-crate;
+        };
+
+        devShells.default = pkgs.mkShell {
+          # Extra inputs can be added here
+          nativeBuildInputs =
+            commonBuildInputs ++
+            (with pkgs;
+            [
+              cargo
+              fenix.packages.${system}.complete.toolchain
+            ] ++ lib.optionals pkgs.stdenv.isDarwin (with darwin.apple_sdk; [
+              frameworks.SystemConfiguration
+            ]));
+        };
+      });
+}
--- scripts/run_qemu.bash
+++ scripts/run_qemu.bash
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 DIR=$(dirname $1)
 mkdir -p $DIR/efi/boot
 cp $1 $DIR/efi/boot/bootx64.efi
 
-qemu-system-x86_64 --enable-kvm -bios /usr/share/ovmf/OVMF.fd -drive file=fat:rw:$DIR,format=raw -nodefaults -display gtk -vga std -cpu host -m 8192
+qemu-system-x86_64-uefi --enable-kvm -drive file=fat:rw:$DIR,format=raw -nodefaults -display gtk -vga std -cpu host -m 8192
